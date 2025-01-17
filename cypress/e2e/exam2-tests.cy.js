4eb0cbb12cb552115a444c59b63cef9f2037751a
imresaks <imresaks@gmail.com>
2025-01-17 13:50:44 +0200

    Task 9 (cypress test)

--- cypress/e2e/exam2-tests.cy.js
+++ cypress/e2e/exam2-tests.cy.js
@@ -1,6 +1,13 @@
 /// <reference types = "cypress"/>  
 
-
+it('Test', () => {
+    cy.visit('http://localhost:8080/Instructor')
+    cy.get('h1').contains('Instructor Management Pannel')
+    cy.get('tbody tr').first().find('input[name="hws"]').clear().type('20')
+    cy.get('tbody tr').first().find('button.update').click()
+    cy.url().should('eq', 'http://localhost:8080/')
+    cy.get('tbody tr').first().find('td').last().click()
+  })
 
 
 
